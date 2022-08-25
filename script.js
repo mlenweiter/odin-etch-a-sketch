c20b6d7dd67f47ca26ce2634dd305c92b349e4e3
mlenweiter <martinlenweiter@protonmail.com>
2022-08-25 12:30:00 +0200

    create grid

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+const grid = document.querySelector('.grid');
+
+for(i = 0;  i < 16; i ++){
+    const div = document.createElement('div');
+    div.textContent = 'hi!'
+    grid.appendChild(div)
+
+}
+

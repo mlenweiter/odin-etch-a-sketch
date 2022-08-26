30d592bc7aaff086fe7623427a7137f63f673ecb
mlenweiter <martinlenweiter@protonmail.com>
2022-08-26 15:23:51 +0200

    done!

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ console.log("hello");
 
 const grid = document.querySelector('.grid');
 
-for(i = 0;  i < 16; i ++){
-    const div = document.createElement('div');
-    grid.appendChild(div)
-    div.style.border ="thin solid black"
-    div.addEventListener('mouseover', function(e){
-        div.style.background = 'black'
-    })
 
 
-}
+const button = document.querySelector('.chooseDimension');
+button.addEventListener('click', function(e){
+    let dimension = prompt('choose your dimension (2-100)')
+    grid.innerHTML = '';
+    for(i = 0;  i < dimension*dimension; i ++){
+        const div = document.createElement('div');
+        grid.appendChild(div)
+        div.style.border ="thin solid black"
+        div.addEventListener('mouseover', function(e){
+            div.style.background = 'black'
+        })
+        grid.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+    }
+})
 
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', function(e){
+    let children = grid.children;
+    for(i = 0; i<children.length; i++){
+        const child = children[i]
+        child.style.background = 'white';
+    }
+})
